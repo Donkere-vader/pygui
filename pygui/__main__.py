cd3430bf8718982d1a910d44f26d63061e204ea7
donkerevader <ctweeduizendvier@yahoo.com>
2020-08-21 21:21:24 +0200

    Switched to jinja2 template

--- pygui/__main__.py
+++ pygui/__main__.py
@@ -1,22 +1,26 @@
 from tkinter import Tk, Frame, Label, Button, Image
 from xmlparser import XMLParser
-from .exceptions import WindowNotFound, ComponentNotFound
+from jinja2 import Environment, PackageLoader, select_autoescape
 import os
+
 
 class PyGui:
     def __init__(self):
         self.parser = XMLParser.XMLParser()
         self.showing_window_vars = None
 
-    def show(self, _window, **kwargs):
-        self.showing_window_vars = kwargs
-        """ Show a specific window """
-        try:
-            xml_str = open(f'templates/windows/{_window}.xml', 'r').read()
-        except FileNotFoundError:
-            raise WindowNotFound(f"The specified window '{_window}' was not found at templates/windows/{_window}.xml")
+        # Jinja2 enviroment
+        self.env = Environment(
+            loader=PackageLoader('pygui', 'templates/'),
+            autoescape=select_autoescape(['xml'])
+        )
 
-        window_xml_str = self.set_variables(xml_str, **kwargs)
+    def show(self, _window, **kwargs):
+        """ Show a specific window """
+        self.showing_window_vars = kwargs
+
+        template = self.env.get_template(f'windows/{_window}.xml')
+        window_xml_str = template.render(variable="test1234")
         window_xml = self.parser.loads(window_xml_str)
         self.construct(window_xml)
 
@@ -38,31 +42,11 @@ class PyGui:
 
     def _construct_component(self, tag):
         """ Returns a Tag object resulting from the component """
-        try:
-            xml_str = open(f'templates/components/{tag.name[1:]}.xml').read()
-        except FileNotFoundError:
-            raise ComponentNotFound(f"No component {tag.name[1:]} found at /templates/components/{tag.name[1:]}.xml")
-
         attrs = self.showing_window_vars
         for attr in tag.attrs:
             attrs[attr] = tag.attrs[attr]
 
-        xml_str = self.set_variables(xml_str, **attrs)
+        template = self.env.get_template(f'components/{tag.name[1:]}.xml')
+        xml_str = template.render(**attrs)
         new_tag = self.parser.loads(xml_str)
         return new_tag
-
-    def set_variables(self, xml_str, **kwargs):
-        """ searches for places with {{ a tag }} like that to replace that with the variable as named inside """
-
-        for var in kwargs:
-            findable_ways = [
-                "{{" + f" {var} " + "}}",
-                "{{" + f"{var}" + "}}",
-                "{{" + f"{var} " + "}}",
-                "{{" + f" {var}" + "}}"
-            ]
-
-            for possible in findable_ways:
-                xml_str = xml_str.replace(possible, kwargs[var])
-
-        return xml_str
