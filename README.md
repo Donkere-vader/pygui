c3bb5dcacd22c8a7babe25732ca879d64ac60306
donkerevader <ctweeduizendvier@yahoo.com>
2020-08-21 17:56:17 +0200

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# PyGui
+A module based on Tkinter that shows windows constructed from xml
+
---
+++ pygui/__main__.py
@@ -0,0 +1,14 @@
+from tkinter import Tk, Frame, Label, Button, Image
+import os
+
+class PyGui:
+    def __init__(self):
+        self.windows = []
+
+    def show(self, window, **kwargs):
+        """ Show a specific window """
+        pass
+
+    def construct(self):
+        """ Construct a component """
+
