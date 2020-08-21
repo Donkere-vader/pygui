00f88b600bb45a1f1852f1a9c971d576c4cbc55f
donkerevader <ctweeduizendvier@yahoo.com>
2020-08-21 19:01:08 +0200

    added set_variable function and other stuff

--- pygui/__main__.py
+++ pygui/__main__.py
@@ -1,14 +1,38 @@
 from tkinter import Tk, Frame, Label, Button, Image
+from xmlparser import XMLParser
+from .exceptions import WindowNotFound, ComponentNotFound
 import os
 
 class PyGui:
     def __init__(self):
-        self.windows = []
+        self.parser = XMLParser.XMLParser()
 
-    def show(self, window, **kwargs):
+    def show(self, window):
         """ Show a specific window """
+        try:
+            xml_str = open(f'templates/windows/{window}.xml', 'r').read()
+        except FileNotFoundError:
+            raise WindowNotFound(f"The specified window '{window}' was not found at templates/windows/{window}.xml")
+
+        window_xml_str = self.set_variables(xml_str)
+
+        window_xml = self.parser.loads(window_xml_str)
+
+    def construct(self, xml_obj):
         pass
 
-    def construct(self):
-        """ Construct a component """
+    def set_variables(self, xml_str, **kwargs):
+        """ searches for places with {{ a tag }} like that to replace that with the variable as named inside """
 
+        for var in self.vars:
+            findable_ways = [
+                "{{" + f" {var} " + "}}",
+                "{{" + f"{var}" + "}}",
+                "{{" + f"{var} " + "}}",
+                "{{" + f" {var}" + "}}"
+            ]
+
+            for possible in findable_ways:
+                xml_str = xml_str.replace(possible, self.vars[var])
+
+        return xml_str
