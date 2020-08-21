5aa3ab7d1a841e6a4ca5166e21374e14a3c65aef
donkerevader <ctweeduizendvier@yahoo.com>
2020-08-21 19:30:32 +0200

    components now work

--- pygui/__main__.py
+++ pygui/__main__.py
@@ -6,20 +6,50 @@ import os
 class PyGui:
     def __init__(self):
         self.parser = XMLParser.XMLParser()
+        self.showing_window_vars = None
 
-    def show(self, window):
+    def show(self, _window, **kwargs):
+        self.showing_window_vars = kwargs
         """ Show a specific window """
         try:
-            xml_str = open(f'templates/windows/{window}.xml', 'r').read()
+            xml_str = open(f'templates/windows/{_window}.xml', 'r').read()
         except FileNotFoundError:
-            raise WindowNotFound(f"The specified window '{window}' was not found at templates/windows/{window}.xml")
+            raise WindowNotFound(f"The specified window '{_window}' was not found at templates/windows/{_window}.xml")
 
-        window_xml_str = self.set_variables(xml_str)
-
+        window_xml_str = self.set_variables(xml_str, **kwargs)
         window_xml = self.parser.loads(window_xml_str)
+        self.construct(window_xml)
+
+        window_xml.print_out()
 
     def construct(self, xml_obj):
-        pass
+        """ Construct the window """
+        self._loop_tag(xml_obj)
+
+    def _loop_tag(self, tag):
+        """ Check if there are any components """
+        if tag.name.startswith('_'):
+            # it is a component
+            new_tag = self._construct_component(tag)
+            tag.replace(new_tag)
+
+        for child in tag.children:
+            self._loop_tag(child)
+
+    def _construct_component(self, tag):
+        """ Returns a Tag object resulting from the component """
+        try:
+            xml_str = open(f'templates/components/{tag.name[1:]}.xml').read()
+        except FileNotFoundError:
+            raise ComponentNotFound(f"No component {tag.name[1:]} found at /templates/components/{tag.name[1:]}.xml")
+
+        attrs = self.showing_window_vars
+        for attr in tag.attrs:
+            attrs[attr] = tag.attrs[attr]
+
+        xml_str = self.set_variables(xml_str, **attrs)
+        new_tag = self.parser.loads(xml_str)
+        return new_tag
 
     def set_variables(self, xml_str, **kwargs):
         """ searches for places with {{ a tag }} like that to replace that with the variable as named inside """
@@ -33,6 +63,6 @@ class PyGui:
             ]
 
             for possible in findable_ways:
-                xml_str = xml_str.replace(possible, self.vars[var])
+                xml_str = xml_str.replace(possible, kwargs[var])
 
         return xml_str
